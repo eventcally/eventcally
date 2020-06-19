68f48bd22fb112f243312c62136da447053767a8
Daniel Grams <grams.daniel@gmail.com>
2020-06-19 15:11:38 +0200

    translation

--- forms/event.py
+++ forms/event.py
@@ -1,15 +1,16 @@
+from flask_babelex import lazy_gettext
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.fields.html5 import DateTimeLocalField
 from wtforms.validators import DataRequired, Optional
 
 class CreateEventForm(FlaskForm):
-    submit = SubmitField("Create event")
-    name = StringField('Name', validators=[DataRequired()])
-    external_link = StringField('Link URL', validators=[Optional()])
-    ticket_link = StringField('Ticket Link URL', validators=[Optional()])
-    description = TextAreaField('Description', validators=[DataRequired()])
-    start = DateTimeLocalField('Start', format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
+    submit = SubmitField(lazy_gettext("Create event"))
+    name = StringField(lazy_gettext('Name'), validators=[DataRequired()])
+    external_link = StringField(lazy_gettext('Link URL'), validators=[Optional()])
+    ticket_link = StringField(lazy_gettext('Ticket Link URL'), validators=[Optional()])
+    description = TextAreaField(lazy_gettext('Description'), validators=[DataRequired()])
+    start = DateTimeLocalField(lazy_gettext('Start'), format='%Y-%m-%dT%H:%M', validators=[DataRequired()])
 
-    place_id = SelectField('Place', validators=[DataRequired()], coerce=int)
-    host_id = SelectField('Host', validators=[DataRequired()], coerce=int)
+    place_id = SelectField(lazy_gettext('Place'), validators=[DataRequired()], coerce=int)
+    host_id = SelectField(lazy_gettext('Host'), validators=[DataRequired()], coerce=int)
