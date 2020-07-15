aabce73f1af55c8ed25f7abd70a68f73eb68320d
Daniel Grams <grams.daniel@gmail.com>
2020-07-15 16:59:30 +0200

    Vielleicht tzinfo gefixt

--- forms/widgets.py
+++ forms/widgets.py
@@ -18,7 +18,7 @@ class CustomDateTimeWidget:
         date = ''
         hour = minute = 0
         if field.data:
-            date_value = field.data.replace(tzinfo=None)
+            date_value = field.data.replace(tzinfo=berlin_tz)
             date = date_value.strftime("%Y-%m-%d")
             hour = date_value.hour
             minute = date_value.minute
