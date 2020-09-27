652af221c77a1e3d9cf163235809e6bdc0deba86
Daniel Grams <grams.daniel@gmail.com>
2020-09-27 09:57:27 +0200

    Remove runlocal.sh with exposed API Key

--- runlocal.sh
+++
@@ -1,4 +0,0 @@
-export DATABASE_URL="postgresql://daniel@localhost/gsevpt"
-export GOOGLE_MAPS_API_KEY="AIzaSyANTJPQRBTZkVgfK4uURO8-rbgrT1rjS0U"
-#pip install -r requirements.txt
-flask run --host 0.0.0.0
