9ec9dc9068994ce8cc641f9850970a313005a63d
Daniel Grams <grams.daniel@gmail.com>
2020-10-05 16:23:17 +0200

    Umkreissuche (nur akt. Pos.)

--- forms/event_date.py
+++ forms/event_date.py
@@ -1,7 +1,8 @@
+from flask import request
 from flask_babelex import lazy_gettext, gettext
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import SelectMultipleField, FieldList, RadioField, DateTimeField, StringField, SubmitField, TextAreaField, SelectField, BooleanField, IntegerField, FormField
+from wtforms import HiddenField, SelectMultipleField, FieldList, RadioField, DateTimeField, StringField, SubmitField, TextAreaField, SelectField, BooleanField, IntegerField, FormField
 from wtforms.fields.html5 import DateTimeLocalField, EmailField
 from wtforms.validators import DataRequired, Optional
 from wtforms.widgets import html_params, HTMLString
@@ -16,7 +17,12 @@ class FindEventDateForm(FlaskForm):
     date_from = CustomDateField(lazy_gettext('From'), validators=[Optional()])
     date_to = CustomDateField(lazy_gettext('to'), validators=[Optional()])
     keyword = StringField(lazy_gettext('Keyword'), validators=[Optional()])
-
     category_id = SelectField(lazy_gettext('Category'), validators=[Optional()], coerce=int)
+    coordinate = HiddenField(validators=[Optional()])
+    location = StringField(lazy_gettext('Location'), validators=[Optional()])
+    distance = IntegerField(lazy_gettext('Distance'), validators=[Optional()])
 
     submit = SubmitField(lazy_gettext("Find"))
+
+    def is_submitted(self):
+        return 'submit' in request.args
