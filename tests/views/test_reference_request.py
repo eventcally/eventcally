47b41d5f5080eb456c80a46f23eada17c40098d1
Daniel Grams <grams.daniel@gmail.com>
2021-01-24 19:46:48 +0100

    After creating an event, the user should be able to submit a reference request #85

--- tests/views/test_reference_request.py
+++ tests/views/test_reference_request.py
@@ -26,7 +26,9 @@ def test_create(client, app, utils, seeder, mocker, db_error):
         utils.assert_response_db_error(response)
         return
 
-    utils.assert_response_redirect(response, "event", event_id=event_id)
+    utils.assert_response_redirect(
+        response, "manage_admin_unit_reference_requests_outgoing", id=admin_unit_id
+    )
     utils.assert_send_mail_called(mail_mock, "other@test.de")
 
     with app.app_context():
