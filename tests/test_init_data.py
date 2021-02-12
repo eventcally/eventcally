0fb69ec3c2bfd6ff32dc119bbd869976e622ec77
Daniel Grams <grams.daniel@gmail.com>
2021-02-12 09:29:50 +0100

    Make Swagger Auth URLs https #107

--- tests/test_init_data.py
+++ tests/test_init_data.py
@@ -1,14 +1,7 @@
-import pytest
-
-
-@pytest.mark.parametrize("insecure", [None, "0", "1"])
-def test_add_oauth2_scheme(app, utils, insecure):
+def test_add_oauth2_scheme(app, utils):
     import os
 
-    if insecure:
-        os.environ["AUTHLIB_INSECURE_TRANSPORT"] = insecure
-    else:
-        del os.environ["AUTHLIB_INSECURE_TRANSPORT"]
+    del os.environ["AUTHLIB_INSECURE_TRANSPORT"]
 
     url = utils.get_url("home")
     utils.get_ok(url)
