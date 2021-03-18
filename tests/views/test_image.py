5364ac16ea6edfe6e96931febcaf5143d6d8305a
Daniel Grams <grams.daniel@gmail.com>
2021-03-18 10:47:58 +0100

    Clear image cache #148

--- tests/views/test_image.py
+++ tests/views/test_image.py
@@ -6,14 +6,12 @@ from project import img_path
 
 
 @pytest.mark.parametrize("size", [None, 100])
-@pytest.mark.parametrize("width", [None, 100])
-@pytest.mark.parametrize("height", [None, 100])
-def test_read(app, seeder, utils, size, width, height):
+def test_read(app, seeder, utils, size):
     user_id, admin_unit_id = seeder.setup_base()
     image_id = seeder.upsert_default_image()
 
     shutil.rmtree(img_path, ignore_errors=True)
 
-    url = utils.get_url("image", id=image_id, s=size, w=width, h=height)
+    url = utils.get_url("image", id=image_id, s=size)
     utils.get_ok(url)
     utils.get_ok(url)  # cache
