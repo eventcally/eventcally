f1b30aed639dc390338b16fb7962848887ce7a44
Daniel Grams <grams.daniel@gmail.com>
2021-07-25 23:23:17 +0200

    Add validation to register form #239

--- tests/views/test_js.py
+++ tests/views/test_js.py
@@ -1,37 +1,34 @@
-def test_js_check_org_short_name(client, seeder, utils):
-    seeder.create_user(admin=True)
-    utils.login()
-
-    url = utils.get_url("admin_unit_create")
+def test_js_js_check_register_email(client, seeder, utils):
+    url = utils.get_url("security.register")
     response = utils.get(url)
 
     with client:
-        url = utils.get_url("js_check_org_short_name")
+        url = utils.get_url("js_check_register_email")
         response = utils.post_form_data(
             url,
             {
-                "short_name": "meinecrew",
+                "email": "test@test.de",
             },
         )
         utils.assert_response_ok(response)
         assert response.data == b"true"
 
 
-def test_js_check_org_short_name_exists(client, seeder, utils):
-    seeder.create_user(admin=True)
-    user_id = utils.login()
-    seeder.create_admin_unit(user_id, "Meine Crew")
+def test_js_js_check_register_email_exists(client, seeder, utils):
+    seeder.create_user()
+    url = utils.get_url("security.register")
+    response = utils.get(url)
 
-    url = utils.get_url("admin_unit_create")
+    url = utils.get_url("js_check_register_email")
     response = utils.get(url)
 
     with client:
-        url = utils.get_url("js_check_org_short_name")
+        url = utils.get_url("js_check_register_email")
         response = utils.post_form_data(
             url,
             {
-                "short_name": "meinecrew",
+                "email": "test@test.de",
             },
         )
         utils.assert_response_ok(response)
-        assert response.data == b'"Der Kurzname ist bereits vergeben"'
+        assert response.data == b'"Mit dieser E-Mail existiert bereits ein Account."'
