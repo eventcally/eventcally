8d0672cff72958e17d2b67a28b6539321586b8a0
Daniel Grams <grams.daniel@gmail.com>
2021-10-14 14:40:09 +0200

    Organisation: Name auf Eindeutigkeit prüfen #309

--- tests/views/test_js.py
+++ tests/views/test_js.py
@@ -41,6 +41,45 @@ def test_js_check_org_short_name_exists(client, seeder, utils):
         assert response.json == "Der Kurzname ist bereits vergeben"
 
 
+def test_js_check_org_name(client, seeder, utils):
+    seeder.create_user(admin=True)
+    utils.login()
+
+    url = utils.get_url("admin_unit_create")
+    response = utils.get(url)
+
+    with client:
+        url = utils.get_url("js_check_org_name")
+        response = utils.post_form_data(
+            url,
+            {
+                "name": "Meine Crew",
+            },
+        )
+        utils.assert_response_ok(response)
+        assert response.json
+
+
+def test_js_check_org_name_exists(client, seeder, utils):
+    seeder.create_user(admin=True)
+    user_id = utils.login()
+    seeder.create_admin_unit(user_id, "Meine Crew")
+
+    url = utils.get_url("admin_unit_create")
+    response = utils.get(url)
+
+    with client:
+        url = utils.get_url("js_check_org_name")
+        response = utils.post_form_data(
+            url,
+            {
+                "name": "Meine Crew",
+            },
+        )
+        utils.assert_response_ok(response)
+        assert response.json == "Der Name ist bereits vergeben"
+
+
 def test_js_js_check_register_email(client, seeder, utils):
     url = utils.get_url("security.register")
     response = utils.get(url)
