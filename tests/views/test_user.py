eddd540945ca4c872be230d2a699ffd904e9f58b
Daniel Grams <grams.daniel@gmail.com>
2021-11-05 15:54:55 +0100

    Hinweis, wenn Einladung für anderen Nutzer #328

--- tests/views/test_user.py
+++ tests/views/test_user.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_profile(client, seeder, utils):
     user_id, admin_unit_id = seeder.setup_base()
     seeder.create_event(admin_unit_id)
@@ -26,17 +29,24 @@ def test_organization_invitation_not_authenticated(client, app, utils, seeder):
     assert response.headers["Location"].startswith("http://localhost/login")
 
 
+@pytest.mark.parametrize("user_exists", [True, False])
 def test_organization_invitation_currentUserDoesNotMatchInvitationEmail(
-    client, app, db, utils, seeder
+    client, app, db, utils, seeder, user_exists
 ):
     _, admin_unit_id = seeder.setup_base()
     invitation_id = seeder.create_admin_unit_invitation(admin_unit_id)
 
-    seeder.create_user("invited@test.de")
+    if user_exists:
+        seeder.create_user("invited@test.de")
+
     url = utils.get_url("user_organization_invitation", id=invitation_id)
-    response = client.get(url)
-    assert response.status_code == 302
-    assert response.headers["Location"] == "http://localhost/profile"
+    environ, response = client.get(url, follow_redirects=True, as_tuple=True)
+
+    assert environ["REQUEST_URI"] == "/profile"
+    utils.assert_response_ok(response)
+    utils.assert_response_contains(
+        response, "Die Einladung wurde für einen anderen Nutzer ausgestellt."
+    )
 
 
 def test_organization_invitation_list(client, seeder, utils):
