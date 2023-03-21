7a2003f377697ee29321939f55c09dd3db5da272
Daniel Grams <grams.daniel@gmail.com>
2023-03-21 22:29:18 +0100

    White label #382

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,16 +1,6 @@
 import "./commands";
 import failOnConsoleError from "cypress-fail-on-console-error";
-import failOnNetworkRequest from "cypress-fail-on-network-request";
 
-const config = {
-    requests: [
-      { status: 200 },
-      { status: 201 },
-      { status: 204 },
-    ],
-};
-
-failOnNetworkRequest(config);
 failOnConsoleError();
 
 before(() => {
