b1fe9d4bfbc117b628deb59662c5af9ec32339e9
Daniel Grams <grams.daniel@gmail.com>
2025-01-01 17:47:57 +0100

    Migrate event organizers to view handler #598

--- cypress/e2e/organizer.cy.js
+++ cypress/e2e/organizer.cy.js
@@ -2,13 +2,13 @@ describe("Event organizer", () => {
   it("creates", () => {
     cy.login();
     cy.createAdminUnit().then(function (adminUnitId) {
-      cy.visit("/manage/admin_unit/" + adminUnitId + "/event_organizers/create");
+      cy.visit("/manage/admin_unit/" + adminUnitId + "/event_organizer/create");
       cy.get("#name").type("Mein Veranstalter");
       cy.screenshot("create");
       cy.get("#submit").click();
       cy.url().should(
         "include",
-        "/manage/admin_unit/" + adminUnitId + "/organizers"
+        "/manage/admin_unit/" + adminUnitId + "/event_organizers"
       );
       cy.screenshot("list");
     });
@@ -23,7 +23,7 @@ describe("Event organizer", () => {
         cy.get("#submit").click();
         cy.url().should(
           "include",
-          "/manage/admin_unit/" + adminUnitId + "/organizers"
+          "/manage/admin_unit/" + adminUnitId + "/event_organizers"
         );
       });
     });
@@ -39,7 +39,7 @@ describe("Event organizer", () => {
         cy.get("#submit").click();
         cy.url().should(
           "include",
-          "/manage/admin_unit/" + adminUnitId + "/organizers"
+          "/manage/admin_unit/" + adminUnitId + "/event_organizers"
         );
       });
     });
